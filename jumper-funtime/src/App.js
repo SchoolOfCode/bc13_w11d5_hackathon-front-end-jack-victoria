a3af657496638baf265f989fdfd24e504c893551
BushraFatimaBF <vglampard@protonmail.com>
2022-12-09 15:51:19 +0000

    tweaks

--- jumper-funtime/src/App.js
+++ jumper-funtime/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Instructions from "./components/Instructions/Instructions"
 import Button from "./components/Button/Button";
 import ToggleButton from "./components/ToggleButton/ToggleButton";
+import ImagesDisplay from "./components/ImageDisplay/ImageDisplay";
 
 function App() {
 
@@ -11,8 +12,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Instructions></Instructions>
-        <Button serving="head" max = "9"/>
-    <Button serving="jumper" max = "9"/> 
+    <ImagesDisplay/>
     </div>
   );
 }
