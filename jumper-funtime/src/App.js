c9573a90ee7abae155fc9c59481befc98330fd73
BushraFatimaBF <vglampard@protonmail.com>
2022-12-09 11:34:01 +0000

    added random number generator hook

--- jumper-funtime/src/App.js
+++ jumper-funtime/src/App.js
@@ -3,7 +3,20 @@ import Button from "./components/Button/Button";
 import ToggleButton from "./components/ToggleButton/ToggleButton";
 
 function App() {
-  return <h1 className="App-logo">JUMPER FUNTIME! (spinning in progress) </h1>;
+
+  
+
+
+  return (
+    <div>
+      <h1 className="App-logo">JUMPER FUNTIME! (spinning in progress) </h1>;
+      {/* <ImageDisplay/>
+  <Button/>
+  <Button/>
+  <Instructions/>
+  <ToggleButton/> */}
+    </div>
+  );
 }
 
 export default App;
